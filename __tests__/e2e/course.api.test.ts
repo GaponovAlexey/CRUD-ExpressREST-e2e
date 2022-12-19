3c5bb08eb805ccb67593d06aaa68f1eb152661fa
kekelberi <alexei.gaponov@gmail.com>
2022-12-19 15:51:10 -0600

    save

--- __tests__/e2e/course.api.test.ts
+++ __tests__/e2e/course.api.test.ts
@@ -1,14 +1,20 @@
-import request from "supertest";
-import { app } from "../../src";
+import request from "supertest"
+import { app, HTTP_STATUSES } from "../../src"
 
-describe("/skille", () => {
-  it("da", async () => {
-    const t = await request(app);
-
-    t.get("/skill").expect(200, [
-      { id: 1, title: "front" },
-      { id: 2, title: "back" },
-      { id: 3, title: "web3" },
-    ]);
-  });
-});
+describe("/", () => {
+  beforeAll(async () => {
+    await request(app).delete("/__test__/data")
+  }) //clean db
+  it("should return 200 and entry array", async () => {
+    await request(app).get("/skill").expect(HTTP_STATUSES.OK_200, [])
+  })
+  it("should return 404 for not existing skill", async () => {
+    await request(app).get("/skill/1").expect(HTTP_STATUSES.NOT_FOUND_404)
+  })
+  it("shouldn't create skill with correct input data 400", async () => {
+    await request(app)
+      .post("/skill")
+      .send({ title: "" })
+      .expect(HTTP_STATUSES.BAD_REQUEST_400)
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express from "express";
+import express from "express"
 
-export const app = express();
+export const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-const port = 3000;
+const port = 3000
 
 const db = {
   courses: [
@@ -12,68 +12,82 @@ const db = {
     { id: 2, title: "back" },
     { id: 3, title: "web3" },
   ],
-};
+}
+
+export const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+}
+
 // app.use(bodyParser.json({ type: 'application/*+json' }))
 app.get("/skill", (req, res) => {
-  let foundCurses = db.courses;
+  let foundCurses = db.courses
 
   if (req.query.title) {
     foundCurses = foundCurses.filter(
       (s) => s.title.indexOf(req.query.title as string) > -1
-    );
+    )
   }
 
-  res.json(foundCurses);
-});
+  res.json(foundCurses)
+})
 
 app.get("/skill/:id", (req, res) => {
-  const findSkill = db.courses.find((c) => c.id === +req.params.id);
+  const findSkill = db.courses.find((c) => c.id === +req.params.id)
 
   if (!findSkill) {
-    res.sendStatus(404);
-    return;
+    res.sendStatus(404)
+    return
   }
 
-  res.json(findSkill);
-});
+  res.json(findSkill)
+})
 
 app.post("/skill", (req, res) => {
   if (!req.body.title) {
-    res.sendStatus(400);
-    return;
+    res.sendStatus(400)
+    return
   }
 
   const createSkill = {
     id: +new Date(),
     title: req.body.title,
-  };
+  }
 
-  db.courses.push(createSkill);
+  db.courses.push(createSkill)
 
-  res.status(201).json(createSkill);
-});
+  res.status(201).json(createSkill)
+})
 
 app.delete("/skill/:id", (req, res) => {
-  db.courses = db.courses.filter((c) => c.id !== +req.params.id);
+  db.courses = db.courses.filter((c) => c.id !== +req.params.id)
 
-  res.sendStatus(204);
-});
+  res.sendStatus(204)
+})
 app.put("/skill/:id", (req, res) => {
   if (!req.body.title) {
-    res.sendStatus(400);
-    return;
+    res.sendStatus(400)
+    return
   }
-  const findSkill = db.courses.find((c) => c.id === +req.params.id);
+  const findSkill = db.courses.find((c) => c.id === +req.params.id)
 
   if (!findSkill) {
-    res.sendStatus(404);
-    return;
+    res.sendStatus(404)
+    return
   }
-  findSkill.title = req.body.title;
+  findSkill.title = req.body.title
 
-  res.sendStatus(204);
-});
+  res.sendStatus(204)
+})
+app.delete("/__test__/data", (req, res) => {
+  db.courses = []
+  res.sendStatus(204)
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
