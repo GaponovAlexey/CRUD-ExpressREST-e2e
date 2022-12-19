85284db9c3d9494d121d08ea422146df3d6f056a
kekelberi <alexei.gaponov@gmail.com>
2022-12-18 19:49:36 -0600

    save

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+const db = {
+  courses: [
+    { id: 1, title: "front" },
+    { id: 2, title: "back" },
+    { id: 3, title: "web3" },
+  ],
+};
+// app.use(bodyParser.json({ type: 'application/*+json' }))
+app.get("/skill", (req, res) => {
+  const qSkill = db.courses.filter(
+    (s) => s.title.indexOf(req.query.title as string) > -1
+  );
+
+  res.json(qSkill.length <= 0 ? db : qSkill);
+});
+
+app.get("/skill/:id", (req, res) => {
+  const findSkill = db.courses.find((c) => c.id === +req.params.id);
+
+  if (!findSkill) {
+    res.sendStatus(404);
+    return;
+  }
+
+  res.json(findSkill);
+});
+
+app.post("/skill", (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(400);
+    return;
+  }
+
+  const createSkill = {
+    id: +new Date(),
+    title: req.body.title,
+  };
+
+  db.courses.push(createSkill);
+
+  res.status(201).json(createSkill);
+});
+
+app.delete("/skill/:id", (req, res) => {
+  
+  db.courses = db.courses.filter((c) => c.id !== +req.params.id);
+
+  res.sendStatus(204);
+});
+app.put("/skill/:id", (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(400);
+    return;
+  }
+  const findSkill = db.courses.find((c) => c.id === +req.params.id);
+
+  if (!findSkill) {
+    res.sendStatus(404);
+    return;
+  }
+  findSkill.title = req.body.title
+
+  res.sendStatus(204);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
