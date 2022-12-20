b68bbf28f1d55fb50ce11127eabb6039265dba71
kekelberi <alexei.gaponov@gmail.com>
2022-12-19 19:19:02 -0600

    save

--- __tests__/e2e/course.api.test.ts
+++ __tests__/e2e/course.api.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest"
 import { app, HTTP_STATUSES } from "../../src"
+import { CreateInputModel } from "../../src/model/createModel"
 
 describe("/", () => {
   beforeAll(async () => {
@@ -12,23 +13,26 @@ describe("/", () => {
     await request(app).get("/skill/1").expect(HTTP_STATUSES.NOT_FOUND_404)
   })
   it("shouldn't create skill with correct input data 400", async () => {
+    const data: CreateInputModel = { title: "" }
     await request(app)
       .post("/skill")
-      .send({ title: "" })
+      .send()
       .expect(HTTP_STATUSES.BAD_REQUEST_400)
   })
   it("shouldn't create skill 400 if bad and 200 after create ", async () => {
+    const data: CreateInputModel = { title: "" }
     await request(app)
-      .post("/skill")
-      .send({ title: "" })
-      .expect(HTTP_STATUSES.BAD_REQUEST_400)
-
+    .post("/skill")
+    .send(data)
+    .expect(HTTP_STATUSES.BAD_REQUEST_400)
+    
     await request(app).get("/skill").expect(HTTP_STATUSES.OK_200, [])
   })
   it("should create skill request 200, and body after create", async () => {
+    const data: CreateInputModel = { title: "big Data" }
     const createResponse = await request(app)
       .post("/skill")
-      .send({ title: "big Data" })
+      .send(data)
       .expect(HTTP_STATUSES.CREATED_201)
 
     expect(createResponse.body).toEqual({
